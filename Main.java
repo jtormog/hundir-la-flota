ba4461e803292912be93848861a51d84fa0d63a8
Josep Tormo <121088986+jtormog@users.noreply.github.com>
2023-12-15 16:22:22 +0000

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
+    enum opciones {
+        FACIL,
+        MEDIO,
+        DIFICIL,
+        CUSTOM
+    }
+    static int tamanyoTablero = 10;
+
+    static char[][] ocultoTablero = new char[tamanyoTablero][tamanyoTablero];
+    static char[][] jugadorTablero = new char[tamanyoTablero][tamanyoTablero];
 
     static final char agua ='-';
     static final char lancha ='L';
@@ -11,83 +25,158 @@ public class Main {
     static final int tamanyoCrucero = 4;
     static final int tamanyoPortaviones = 5;
 
-    static int lanchasCantidad = 5;
-    static int barcosCantidad = 3;
-    static int crucerosCantidad = 1;
-    static int portavionesCantidad = 1;
+    static int lanchasCantidad;
+    static int barcosCantidad;
+    static int crucerosCantidad;
+    static int portavionesCantidad;
 
-    static char[][] tablero = new char[10][10];
+    static int intentos = 50;
+
     public static void main(String[] args) {
+        boolean jugando;
+        seleccionDeDificultad();
+        llenarTablero();
+        //mostrarTableroOculto();
+        do{
+            System.out.println("Intentos restantes: "+intentos);
+            if (inputUsuario() == 8) break;
+            mostrarTableroJugador();
 
-        for (int i = 0; i < tablero.length; i++){
-            for (int j = 0; j < tablero[i].length; j++){
-                tablero[i][j] = agua;
+            intentos--;
+            jugando = (intentos > 0);
+
+        }while (jugando);
+        if (intentos == 0) System.out.println("Has perdido");
+        else System.out.println("Has ganado");
+        
+
+    }
+    static int inputUsuario(){
+
+        Scanner input = new Scanner(System.in);
+        boolean valido = false;
+        int opcion = 0;
+
+        while (!valido) {
+            try {
+                opcion = input.nextInt();
+                valido = true;
+            } catch (Exception e) {
+                System.out.println("No es un numero valido");
+                input.nextLine();
             }
         }
-        llenarTablero();
+        
+        return opcion;
+    }
+    static void seleccionDeDificultad(){
 
-        for (int i = 0; i < tablero.length; i++){
-            if(i>0)System.out.print(i+"\t");
-            else System.out.print("\n");
+        System.out.println("Selecciona una dificultad:\n\n1. Facil\n2. Medio\n3. Dificil\n4. Custom\n");
+        int opcion = inputUsuario()-1;
+        opciones dificultad = opciones.values()[opcion];
+        boolean seleccionado = false;
 
-            if (i == 0) {
-                for (int j = 0; j < tablero[i].length; j++){
-                    if (j == 0) System.out.print("\t"+j+"\t");
-                    else System.out.print(j+"\t");
-                }
-            }else for (int j = 0; j < tablero[i].length; j++){
-                    System.out.print(tablero[i][j] + "\t");
-            }System.out.println("\n");
+        while (!seleccionado) {
+                switch (dificultad) {
+                    case FACIL:
+                        System.out.println("Modo Facil");
+                        lanchasCantidad = 5;
+                        barcosCantidad = 3;
+                        crucerosCantidad = 1;
+                        portavionesCantidad = 1;
+                        seleccionado = true;
+                        break;
+                    case MEDIO:
+                        System.out.println("Modo Medio");
+                        lanchasCantidad = 2;
+                        barcosCantidad = 1;
+                        crucerosCantidad = 1;
+                        portavionesCantidad = 1;
+                        seleccionado = true;
+                        break;
+                    case DIFICIL:
+                        System.out.println("Modo Dificil");
+                        lanchasCantidad = 1;
+                        barcosCantidad = 1;
+                        seleccionado = true;
+                        break;
+                    case CUSTOM:
+                        System.out.println("Modo Custom");
+                        seleccionado = true;
+                        break;
+                    default:
+                        System.out.println("No es una opcion valida");
+                        break;
+            }   
         }
     }
-    
-    
     static void llenarTablero() {
+
+        for (int i = 0; i < ocultoTablero.length; i++){
+            for (int j = 0; j < ocultoTablero[i].length; j++){
+                ocultoTablero[i][j] = agua;
+                jugadorTablero[i][j] = agua;
+            }
+        }
+
         boolean hayBarcos;
-        int totalBarcos = lanchasCantidad + barcosCantidad + crucerosCantidad + portavionesCantidad;
+
         char tipoBarco = lancha;
 
         do {
 
             if (tipoBarco == lancha) {
                 lanchaPosicionAleatoria();
-                totalBarcos--;
                 tipoBarco = barco;
             } else if (tipoBarco == barco) {
                 barcoPosicionAleatoria();
-                totalBarcos--;
                 tipoBarco = crucero;
             } else if (tipoBarco == crucero) {
                 cruceroPosicionAleatoria();
-                totalBarcos--;
                 tipoBarco = portaviones;
             } else if (tipoBarco == portaviones) {
                 portavionesPosicionAleatoria();
-                totalBarcos--;
             }
+            int totalBarcos = lanchasCantidad + barcosCantidad + crucerosCantidad + portavionesCantidad;
             hayBarcos = (totalBarcos > 0);
     
         }while (hayBarcos);
         return;
+    }
+    static void mostrarTableroJugador() {
+
         
+        for (int i = 0; i < jugadorTablero.length; i++){
+            if(i>0)System.out.print(i+"\t");
+            else System.out.print("\n");
+
+            if (i == 0) {
+                for (int j = 0; j < jugadorTablero[i].length; j++){
+                    if (j == 0) System.out.print("\t"+j+"\t");
+                    else System.out.print(j+"\t");
+                }
+            }else for (int j = 0; j < jugadorTablero[i].length; j++){
+                    System.out.print(jugadorTablero[i][j] + "\t");
+            }System.out.println("\n");
+        }return;
     }
     static void lanchaPosicionAleatoria(){
 
-    int xEjePosicionAleatoria = 0;
-    int yEjePosicionAleatoria = 0;
+        int xEjePosicionAleatoria = 0;
+        int yEjePosicionAleatoria = 0;
 
-        while (lanchasCantidad > 0) {
+            while (lanchasCantidad > 0) {
 
-            xEjePosicionAleatoria = (int) (Math.random() * 10);
-            yEjePosicionAleatoria = (int) (Math.random() * 10);
+                xEjePosicionAleatoria = (int) (Math.random() * 10);
+                yEjePosicionAleatoria = (int) (Math.random() * 10);
 
-            if (tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) {
-                    tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = lancha;
-                    
-            }
-            lanchasCantidad--;
-                    
-        }return;
+                if (ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) {
+                        ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = lancha;
+                        
+                }
+                lanchasCantidad--;
+                        
+            }return;
 
     }
     static void barcoPosicionAleatoria(){
@@ -100,7 +189,7 @@ public class Main {
             xEjePosicionAleatoria = (int) (Math.random() * 10);
             yEjePosicionAleatoria = (int) (Math.random() * 10);
 
-            if (tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = barco;
+            if (ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = barco;
             barcosCantidad--;
                     
         }return;
@@ -116,8 +205,7 @@ public class Main {
             xEjePosicionAleatoria = (int) (Math.random() * 10);
             yEjePosicionAleatoria = (int) (Math.random() * 10);
 
-            if (tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = crucero;
-            
+            if (ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = crucero;
             crucerosCantidad--;
         }return;
 
@@ -132,11 +220,17 @@ public class Main {
             xEjePosicionAleatoria = (int) (Math.random() * 10);
             yEjePosicionAleatoria = (int) (Math.random() * 10);
 
-            if (tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) tablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = portaviones;
+            if (ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] == agua) ocultoTablero[xEjePosicionAleatoria][yEjePosicionAleatoria] = portaviones;
             
             portavionesCantidad--;
                     
             }return;
 
+    }
+    static void mostrarTableroOculto() {
+
+        for (int i = 0; i < ocultoTablero.length; i++){
+         System.out.println(Arrays.toString(ocultoTablero[i]));
+        }return;
     }
 }
